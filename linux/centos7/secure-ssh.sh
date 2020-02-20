2590a1526d16397a98d3ab1ce2f7e4d6ca7cacd2
brian-anderson01 <brian.anderson01@mymail.champlain.edu>
2020-02-20 15:59:27 +0000

    made script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,9 @@
 #adds public key fom the local repo or curled from remote repo
 #removes roots ablility to ssh in
 echo "code goes here"
+sudo useradd -m -d /home/sys265 -s /bin/bash sys265
+sudo mkdir /home/sys265/ .ssh
+sudo cp ~/SYS-265-Tech-Journal/linux/public-keys/id_rsa.pub /home/sys265/ .ssh/authorized_keys
+sudo chmod 700 /home/sys265/ .ssh
+sudo chmod 600 /home/sys265/ .ssh/authorized_keys
+sudo chown -R sys265:sys265 /home/sys265/ .ssh
