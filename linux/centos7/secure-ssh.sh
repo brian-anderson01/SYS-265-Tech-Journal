4fa056947d259baa0ea12d4337e29610933c605f
brian-anderson01 <brian.anderson01@mymail.champlain.edu>
2020-02-20 16:42:34 +0000

    finalised script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
 #secure-ssh.sh
 #author brian.anderson01
 #creates new ssh user using $l parameter
 #adds public key fom the local repo or curled from remote repo
 #removes roots ablility to ssh in
-echo "code goes here"
-sudo useradd -m -d /home/sys265 -s /bin/bash sys265
-sudo mkdir /home/sys265/.ssh
-sudo cp ~/SYS-265-Tech-Journal/linux/public-keys/id_rsa.pub /home/sys265/.ssh/authorized_keys
-sudo chmod 700 /home/sys265/.ssh
-sudo chmod 600 /home/sys265/.ssh/authorized_keys
-sudo chown -R sys265:sys265 /home/sys265/.ssh
+read -p "Enter the name of the new user: " uservar
+echo "Creating user $uservar"
+sleep 1
+sudo useradd -m -d /home/$uservar -s /bin/bash $uservar
+sudo mkdir /home/$uservar/.ssh
+sudo cp ~/SYS-265-Tech-Journal/linux/public-keys/id_rsa.pub /home/$uservar/.ssh/authorized_keys
+sudo chmod 700 /home/$uservar/.ssh
+sudo chmod 600 /home/$uservar/.ssh/authorized_keys
+sudo chown -R $uservar:$uservar /home/$uservar/.ssh
