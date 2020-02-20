88452165214f8cc5079abf906035397b79fbaede
brian-anderson01 <brian.anderson01@mymail.champlain.edu>
2020-02-20 10:39:43 -0500

    added secure ssh file

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author brian.anderson01
+#creates new ssh user using $l parameter
+#adds public key fom the local repo or curled from remote repo
+#removes roots ablility to ssh in
+echo "code goes here"
